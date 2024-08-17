dd1026afb573afc7a6f239e497bbe0f8a4965267
Humoyunmirzo2012 <webdevoper0509@gmail.com>
2024-08-17 11:30:46 +0500

    o-zlashtirish

---
+++ js/app.js
@@ -0,0 +1,51 @@
+let text = "  Salom, dunyo!  ";
+console.log(text);
+console.log(typeof text);
+
+let text2 = "JavaScript Dasturlash Tili";
+console.log(text2);
+console.log(typeof text2);
+
+let text3 = "JavaScript dasturlash tilida kod yozish juda qiziqarli";
+console.log(text3);
+console.log(typeof text3);   
+
+let text4 = "Men kitob o'qishni yaxshi ko'raman";
+let eskiSoz = "kitob";
+let yangiSoz = "film";
+console.log(text4);
+console.log(eskiSoz);
+console.log(yangiSoz);
+
+
+console.log(typeof text4);   
+console.log(typeof eskiSoz);   
+console.log(typeof yangiSoz);   
+
+
+let text5 = "Telefon raqamingiz: +998907744745";
+console.log(text5);
+
+console.log(typeof text5);   
+
+// murakablari
+
+let text6 = "javascript dasturlash tili";
+// Natija: "Javascript Dasturlash Tili"
+console.log(text6);
+
+
+
+let text7 = "Bugun Javascriptda string metodlarini o'rganamiz";
+// Natija: "string"
+console.log(text7);
+
+
+
+let text8 = "tut";
+// Natija: true
+console.log(text8);
+
+let text9 = "JavaScript bir necha yo'nalishda ishlatiladi";
+// Natija: [10, 3, 5, 10, 12]
+console.log(text9);
